a75412c10d34da4f88e95f28944899837399dd45
BrolinsZack <zackbrolins@gmail.com>
2023-02-22 14:40:27 -0800

    task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print last digit
+ *
+ * @i: function parameter
+ *
+ * Return: always r
+ *
+ */
+
+int print_last_digit(int i)
+{
+	int r;
+
+	r = 1 % 10;
+	if (i < 10)
+		r = -r;
+	_putchar (r + '0');
+	return (r);
+}
