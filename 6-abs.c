4c046392ad2ca35d9d25ccab2893a2a2045e5312
BrolinsZack <zackbrolins@gmail.com>
2023-02-23 02:56:10 -0800

    absolute value

---
+++ 6-abs.c
@@ -0,0 +1,14 @@
+include "main.h"
+
+/**
+ * _abs - checks for an absolute value
+ * @b: parameter
+ * Return: absolute value of a number
+ */
+
+int _abs(int b)
+{
+	if (b < 0)
+		a = -b;
+	return (b);
+}
