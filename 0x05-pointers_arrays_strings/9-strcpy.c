6716d1a3405b8490d2e28e79c0ff40a65deb15ed
BrolinsZack <zackbrolins@gmail.com>
2023-02-28 07:40:43 -0800

    task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -5,13 +5,16 @@
  * @src: Source value
  * Return: Pointer to dest
  */
-char *_strcpy(char *dest, char *src)
+char *_strcpy(char *dest, const char *src)
 {
 	int i;
-	for (i = 0 ; src[i] != '\0' ; i++)
+
+	for (i = 0; src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
 	}
-	dest[i++] = '\0';
+
+	dest[i] = '\0';
+
 	return (dest);
 }
