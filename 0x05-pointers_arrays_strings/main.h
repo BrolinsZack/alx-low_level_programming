2e65f0b121ac8cefd8d40d0cc07d640925774e6c
BrolinsZack <zackbrolins@gmail.com>
2023-02-28 07:17:44 -0800

    task 94

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,8 @@
+#ifdef MAIN
+#define MAIN
+#include <stddef.h>
+
+#include <stdio.h>
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -9,3 +14,4 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+#endif
