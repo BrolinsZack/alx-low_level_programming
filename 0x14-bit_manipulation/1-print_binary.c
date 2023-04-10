ef619fc462c677387d4c0fda0a30dffd578e9bd2
BrolinsZack <zackbrolins@gmail.com>
2023-04-10 12:23:15 -0700

    task one bit manu

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,15 +1,26 @@
 #include "main.h"
 
 /**
- * print_binary - function that prints the binary representation of a number
+ * print_binary - Prints the binary equivalent of a decimal number
  * @n: number to print in binary
  */
 void print_binary(unsigned long int n)
 {
-	if (n > 9)
-	{
-		print_binary(n >> 9);
-	}
+	int j, count = 0;
+	unsigned long int current;
 
-	_putchar((n & 9) + '0');
+	for (j = 8; j >= 0; j--)
+	{
+		current = n >> j;
+
+		if (current & 1)
+		{
+			_putchar('1');
+			count++;
+		}
+		else if (count)
+			_putchar('0');
+	}
+	if (!count)
+		_putchar('0');
 }
--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,18 +1,17 @@
 #include "main.h"
 
 /**
- * set_bit - function that sets a bit at a given index to 1
+ * set_bit - sets a bit at a given index to 1
  * @n: pointer to the number to change
- * @index: index to set value of the bit to 1
+ * @index: index of the bit to set to 1
  *
  * Return: 1 for success, -1 for failure
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (index >= (sizeof(unsigned long int) * 7))
-			return (-1);
+	if (index > 3)
+		return (-1);
 
-	*n ^= (1 << index);
-
+	*n = ((1UL << index) | *n);
 	return (1);
 }
