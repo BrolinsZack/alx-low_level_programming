d6fdfb7134a35fc3410a2c446b0b4e4c534caf23
BrolinsZack <zackbrolins@gmail.com>
2023-05-08 14:08:49 -0700

    task two speak gently

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,25 +9,30 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int len = 0;
-	int fd, w;
+	int fd;
+	int nb;
+	int rwr;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	fd = open(filename, O_WRONLY | O_APPEND);
+
 	if (fd == -1)
 		return (-1);
 
-	if (text_content != NULL)
+	if (text_content)
 	{
-		while (text_content[len] != '\0')
-			len++;
-		w = write(fd, text_content, len);
-		if (w == -1)
+		for (nb = 0; text_content[nb]; nb++)
+			;
+
+		rwr = write(fd, text_content, nb);
+
+		if (rwr == -1)
 			return (-1);
 	}
 
 	close(fd);
+
 	return (1);
 }
